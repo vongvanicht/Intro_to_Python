5154f766721bf800469110496f555c7e16057772
hphilamore <hemmaphilamore@gmail.com>
2018-05-28 15:22:09 +0900

    class 5

---
+++ sample_data/library_homework_example/my_project/my_program.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 28 01:35:51 2018
+
+@author: hemma
+"""
+
+import sys
+sys.path.append('../')
+import my_library
+
+my_library.my_func(2, 4)
